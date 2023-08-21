2617ef9c531da46b6a5c6f6c19b39550c530e122
vance <raychang223@gmail.com>
2023-08-21 21:15:08 +0800

    update 1

--- problem_21.cpp
+++ problem_21.cpp
@@ -1,6 +1,18 @@
 // https://www.luogu.com.cn/problem/P1217
 #include <bits/stdc++.h>
+#define endl '\n'
 using namespace std;
+vector<long long> ans;
+int pow_self(int x,int y){
+    int res=1;
+    while(y){
+        if(y&1)
+            res*=x;
+        y>>=1;
+        x*=x;
+    }
+    return res;
+}
 int digits(int x){
     int res=0;
     while(x>0){
@@ -9,25 +21,52 @@ int digits(int x){
     }
     return res;
 }
-int dfs(int i,int deep,int &path,const int &a,const int &b){
+void check(int path){
+    if(path<2)
+        return ;
+    for(int i=2;i<=sqrt(path);i++)
+        if(path%i==0)
+            return ;
+    ans.push_back(path);
+    // cout<<path<<endl;
+}
+void dfs(int i,int deep,int &path,const int &a,const int &b){
     if(deep==i){
-        if(check(path))
-            return 1;
-        return 0;
+        if(a<=path && path<=b)
+            check(path);
+        return ;
     }
-    if(i%2!=0){
-        for(int j=0;j<10;j++)
-            path=j+10^(i/2);
+    if(deep==0 && i%2!=0){
+        for(int j=0;j<10;j++){
+            path=j;
+            dfs(i,deep+1,path,a,b);
+            path=0;
+        }
     }
     else{
-
+        int start=0,step=1;
+        if(deep+2==i)
+            step=2,start=1;
+        for(int j=start;j<10;j+=step){
+            path+=j*pow_self(10,deep);
+            path*=10;
+            path+=j;
+            dfs(i,deep+2,path,a,b);
+            path/=10;
+            path-=j*pow_self(10,deep);
+        }
     }
 }
-int main(){
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
     int a,b,path=0;
     cin>>a>>b;
     int n=digits(a),m=digits(b);
     for(int i=n;i<=m;i++)
-        dfs(i,path,a,b);
+        dfs(i,0,path,a,b);
+    sort(ans.begin(),ans.end());
+    for(int aa:ans)
+        cout<<aa<<endl;
     return 0;
 }
